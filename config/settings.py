7c1761b0f19b88a680fc3ca03dffeacfbdf697e4
AtilaGoesSK <atilagoes.tech@gmail.com>
2026-03-06 20:58:38 -0300

    Secret no Render

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zf4r00kfu_yc)iimp(*(u3cc@9wdp8nmnkhy$hi(_6np4-5h8q'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
